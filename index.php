0e5ddb066c0dd903477c27ed6f40e27295951892
ubiraildo lucas <ubiraildolucas@gmail.com>
2025-01-08 00:18:52 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+    <div class="fundo">
+        <div class="login">
+            <h2>Bem-vindo</h2>
+            <form>
+                <label class="label" for="email">E-mail</label>
+                <input type="text" id="email" required>
+                <label class="label" for="senha">Senha</label>
+                <input type="password" id="senha" required>
+                <a href="#">Esqueceu sua senha? / recuperar senha</a>
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+        <p id="starcode"><a href="">@StarCode</a></p>
+    </div>
+    <div class="imagem-fixa">
+        <div class="eclipse"></div>
+        <img src="img/menino.png" alt="Logo">
+    </div>
 </body>
+
 </html>
