9c8e476a66e3843f66915964d3425a35ac5fbcaa
Jeovane venâncio <40653032+JeovaneVenic@users.noreply.github.com>
2025-01-22 21:28:51 -0300

    Tela-de-painel-e-cadastro
    Realização da tela de painel e de cadastro e algumas alterações no login.

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -2,21 +2,21 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="../css/cadastro.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cadastro</title>
 </head>
 <body>
-<body style="background-image: url('./img/fundopiscina.png'); background-size: cover; background-repeat: no-repeat;">
+<body style="background-image: url('../img/fundopiscina.png'); background-size: cover; background-repeat: no-repeat;">
     <div class="fundo">
         <div class="login">
-            <h2>Bem-vindo</h2>
+            <h2>Cadastre-se</h2>
             <form>
                 <label class="label" for="email">E-mail:</label>
                 <input type="text" id="email" required>
                 <label class="label" for="senha">Senha:</label>
                 <input type="password" id="senha" required>
-                <a href="http://localhost/weezy/pages/cadastro" style="text-decoration: underline; color: white;" onmouseover="this.style.color='blue';" onmouseout="this.style.color='white';">Esqueceu sua senha? / Recuperar senha.</a>
+                <a href="http://localhost/weezy/" style="text-decoration: underline; color: white;" onmouseover="this.style.color='blue';" onmouseout="this.style.color='white';">Já possui uma conta? / Entrar.</a>
                 <button type="submit">Entrar</button>
             </form>
         </div>
@@ -24,7 +24,7 @@
     </div>
     <div class="imagem-fixa">
         <div class="eclipse"></div>
-        <img src="img/menino.png" alt="Logo">
+        <img src="../img/menino.png" alt="Logo">
     </div>
 </body>
 
---
+++ pages/painel.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+    <link rel="stylesheet" href="../css/painel.css"> <!-- Link para o arquivo CSS externo -->
+</head>
+<body style="background-image: url('../img/fundopiscina.png'); background-size: cover; background-repeat: no-repeat;">
+    <h1>Painel</h1>
+
+    <!-- Div para os botões -->
+    <div class="botoes-container">
+    <button class="botao1">
+    <img src="../img/cadastro.png" alt="icone 1" class="icone">
+    <span>Cadastro</span>
+    </button>
+    <button class="botao2">
+        <img src="../img/pagamento.png" alt="Icone 2">
+        <span>Pagamento</span>
+    </button>
+    <button class="botao3">
+        <img src="../img/alunos.png" alt="Icone 3">
+        <span>Alunos</span>
+    </button>
+</div>
+</body>
+</html>
